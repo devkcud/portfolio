617c70856d71e52928ccf86724708842105b2ec5
André devkcud Albanese <patommmmm@proton.me>
2023-12-15 22:04:38 -0300

    feat: Add home page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import 'virtual:uno.css';
+  import '$lib/styles/global.css';
+</script>
+
+<main class="min-h-screen flex justify-center items-center">
+  <slot />
+</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,35 @@
+<script lang="ts">
+  import { LinkSolid as Link } from 'flowbite-svelte-icons';
+
+  const github = 'https://github.com/devkcud';
+</script>
+
 <svelte:head>
   <title>Home</title>
 </svelte:head>
 
-<h1>Hello, world!</h1>
+<section class="select-none flex flex-col gap-4 animate-fade-in">
+  <h1 class="fw-400 text-5xl">
+    Hey, I'm
+    <a
+      href={github}
+      class="inline-flex items-center gap-2 color-purple decoration-none animate-underline"
+    >
+      André
+      <Link />
+    </a>.
+  </h1>
+
+  <p>
+    I am also known as <span class="color-purple">devkcud</span> and
+    <span class="color-purple">Pato</span>.
+  </p>
+
+  <ul class="flex justify-center gap-2 border-t-1 border-t-solid border-white pt-2 list-none op-30">
+    <li>Student from São Paulo, Brazil</li>
+    <li>•</li>
+    <li>Fullstack Developer</li>
+    <li>•</li>
+    <li>Game developer</li>
+  </ul>
+</section>
